1a71006884778bfb6e58c885149702fb05fe8db5
LamaAlshater <Lamash207@gmail.com>
2025-06-22 11:55:54 -0700

    Delete Personal websit1/js/chat.js

--- Personal websit1/js/chat.js
+++
@@ -1,1 +0,0 @@
-window.$crisp=[];window.CRISP_WEBSITE_ID="0f52512d-77f9-40ec-b7f9-1579bda3779d";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
